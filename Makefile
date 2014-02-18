1b0e3e91320b9e44fa3f3eddffe2d60d0926ea88
Yusuke Suzuki <utatane.tea@gmail.com>
2014-02-19 00:56:12 +0900

    Use str_copy instead of strdup
    Since strdup is not standardized in ANSI C, use str_copy instead.

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 
+SRC := strsplit.c
+SRC += deps/*/*.c
+
 all: clean test
 
 clean:
 	rm -f strsplit-test
 
 test:
-	$(CC) strsplit.c test.c -std=c99 -o strsplit-test
+	$(CC) $(SRC) test.c -std=c99 -o strsplit-test -Ideps
 	./strsplit-test
--- strsplit.c
+++ strsplit.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "strsplit.h"
+#include "str-copy/str-copy.h"
 
 int
 strsplit (const char *str, char *parts[], const char *delimiter) {
   char *pch;
   int i = 0;
-  char *tmp = strdup(str);
+  char *tmp = str_copy(str);
   pch = strtok(tmp, delimiter);
 
-  parts[i++] = strdup(pch);
+  parts[i++] = str_copy(pch);
 
   while (pch) {
     pch = strtok(NULL, delimiter);
     if (NULL == pch) break;
-    parts[i++] = strdup(pch);
+    parts[i++] = str_copy(pch);
   }
 
   free(tmp);
